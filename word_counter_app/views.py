4affa7fad4d3677e3c79a1b9c8f6495558807c63
Emile Durham <durhamemile@gmail.com>
2021-04-30 13:50:13 -0700

    plotly graph fixes

--- word_counter_app/views.py
+++ word_counter_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Count
 from .models import T1, T2, QC
 from .forms import T1Form, T2Form, QCForm
 from django.contrib.auth.decorators import login_required
@@ -252,54 +253,6 @@ def delete_qc(request):
 
 @login_required(login_url='/accounts/login')
 def dashboard(request):
-    try:
-        os.chdir('word_counter_app/templates')
-    except:
-        pass
-
-    id_list = 0
-    date_list = []
-    for item in T1.objects.values():
-        if item['id']:
-            id_list += 1
-        date_list.append(item['date'])
-
-    id_list2 = 0
-    date_list2 = []
-    for item2 in T2.objects.values():
-        if item2['id']:
-            id_list2 += 1
-        date_list2.append(item2['date'])
-
-    id_list3 = 0
-    date_list3 = []
-    for item3 in QC.objects.values():
-        if item3['id']:
-            id_list3 += 1
-        date_list3.append(item3['date'])
-
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=[i for i in date_list], y=[i for i in range(id_list)], name='T1'))
-    fig.add_trace(go.Scatter(x=[i for i in date_list2], y=[i for i in range(id_list2)], name='T2'))
-    fig.add_trace(go.Scatter(x=[i for i in date_list3], y=[i for i in range(id_list3)], name='QC'))
-    fig.update_layout(yaxis=dict(range=[0, [i for i in range(id_list)]]),
-                      xaxis=dict(range=[date.today(), [i.date() for i in date_list]]),
-                      )
-    fig.write_html('graph.html')
-    graph = fig.to_html()
-
-    fig2 = go.Figure()
-    fig2.add_trace(go.Bar(x=[i.date() for i in date_list], y=[len(date_list)], text=[len(date_list)], textposition='auto', name='T1'))
-    fig2.add_trace(go.Bar(x=[i.date() for i in date_list2], y=[len(date_list2)], text=[len(date_list2)], textposition='auto', name='T2'))
-    fig2.add_trace(go.Bar(x=[i.date() for i in date_list3], y=[len(date_list3)], text=[len(date_list3)], textposition='auto', name='QC'))
-    fig2.write_html('graph2.html')
-    graph2 = fig2.to_html()
-
-    fig3 = go.Figure()
-    fig3.add_trace(go.Pie(labels=['T1','T2', 'QC'], values=[len(date_list),len(date_list2), len(date_list3)], textposition='auto'))
-    fig3.write_html('graph3.html')
-    graph3 = fig3.to_html()
-
     date_count_t1 = len(T1.objects.all())
     date_count_t2 = len(T2.objects.all())
     date_count_qc = len(QC.objects.all())
@@ -309,6 +262,43 @@ def dashboard(request):
     single_date_count_t2_2 = single_date_count_t2 * 2
     single_date_sum = single_date_count_t1 + single_date_count_t2_2 + single_date_count_qc
     today = datetime.datetime.today()
+
+    try:
+        os.chdir('word_counter_app/templates')
+    except:
+        pass
+
+    date_list_t1 = [item['date'] for item in T1.objects.values()]
+    date_list_t2 = [item['date'] for item in T2.objects.values()]
+    date_list_qc = [item['date'] for item in QC.objects.values()]
+
+    id_list = [item['id'] for item in T1.objects.values()]
+    id_list2 = [item['id'] for item in T2.objects.values()]
+    id_list3 = [item['id'] for item in QC.objects.values()]
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=date_list_t1, y=id_list, name='T1'))
+    fig.add_trace(go.Scatter(x=date_list_t2, y=id_list2, name='T2'))
+    fig.add_trace(go.Scatter(x=date_list_qc, y=id_list3, name='QC'))
+
+    fig.write_html('graph.html')
+    graph = fig.to_html()
+
+    fig2 = go.Figure()
+    fig2.add_trace(go.Bar(x=[i.date() for i in date_list_t1], y=date_list_t1, text=[], textposition='auto', name='T1'))
+    fig2.add_trace(go.Bar(x=[i.date() for i in date_list_t2], y=date_list_t2, text=[], textposition='auto', name='T2'))
+    fig2.add_trace(go.Bar(x=[i.date() for i in date_list_qc], y=date_list_qc, text=[], textposition='auto', name='QC'))
+    fig2.update_layout(yaxis=dict(range=[0, date_list_t1]),
+                      xaxis=dict(range=[date.today(), date_list_t1]),
+                      )
+
+    fig2.write_html('graph2.html')
+    graph2 = fig2.to_html()
+
+    fig3 = go.Figure()
+    fig3.add_trace(go.Pie(labels=['T1','T2', 'QC'], values=[len(date_list_t1),len(date_list_t2), len(date_list_qc)], textposition='auto'))
+    fig3.write_html('graph3.html')
+    graph3 = fig3.to_html()
 
     return render(request, 'dashboard.html', {'single_date_count_t1': single_date_count_t1,
                                               'single_date_count_t2': single_date_count_t2,
